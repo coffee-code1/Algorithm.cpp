30e08391bf33cb0bfbc2c51955b3fc2a1dd9e8be
coffee-code1 <qwertLU20061122@git>
2025-11-22 19:58:14 +0800

    注释的加入

--- BFS_multiple.cpp
+++ BFS_multiple.cpp
@@ -15,17 +15,17 @@ int dy[4]={0,1,0,-1};
 void bfs(){
   memset(dis,-1,sizeof dis);
   queue<Node> q;
-  for(int i=0; i<n; i++)
+  for(int i=0; i<n; i++)//多源的起始点输入
     for(int j=0; j<m; j++)
       if(g[i][j] == '1')
-        dis[i][j]=0, q.push({i,j});
+        dis[i][j]=0, q.push({i,j});//输入后初始化为0；一是防止走回头路，二是要计算距离这个点的距离
   while(q.size()){
     auto t=q.front(); q.pop();
     for(int i=0; i < 4; i++){
       int a=t.x+dx[i], b=t.y+dy[i];
       if(a<0||a>=n||b<0||b>=m)continue;
       if(dis[a][b]!=-1) continue;
-      dis[a][b]=dis[t.x][t.y]+1;
+      dis[a][b]=dis[t.x][t.y]+1;//一次加一代表距离
       q.push({a,b});
     }
   }
